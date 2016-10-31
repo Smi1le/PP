06357e5e12053d987d3177d58bb15c1e3ed12d7d
Smi1le <tima55510@gmail.com>
2016-10-31 20:42:10 +0300

    Выполнены пункты 5 и 6: 5.   Количество одновременно работающих экземпляров CBankClient должно определяться параметром приложения. 6.   Приложение должно выдавать справку по своему использованию при запуске с ключом /? .

--- Task2_Bank/PP2/PP2.cpp
+++ Task2_Bank/PP2/PP2.cpp
@@ -1,18 +1,66 @@
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 #include "CBank.h"
 #include "CBankClient.h"
 
-int main()
+void DisplayUsageHelp()
 {
+	std::cout << "::::::::A reminder to use the program with the key::::::::\n\n\n" << std::endl;
+	std::cout << "when using the program with a data synchronization program \n will work properly without it - no" << std::endl;
+	std::cout << "WARNINGS!!!!! Use with key !!!!! Use without a key \nfor informational purposes only !!!!!" << std::endl;
+}
+
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		std::cerr << "Error! Usage program.exe <client number> " << std::endl;
+		return EXIT_FAILURE;
+	}
+	DisplayUsageHelp();
+	bool key = false;
+	std::string keyValue;
+	while (true)
+	{
+		std::cout << "Please enter whether you want to use synchronization primitives or not (Y/N) : ";
+		std::getline(std::cin, keyValue);
+		if (keyValue == "Y" || keyValue == "y")
+		{
+			key = true;
+			break;
+		}
+		else if (keyValue == "N" || keyValue == "n")
+		{
+			key = false;
+			break;
+		}
+		else
+		{
+			std::cout << "You enter an incorrect key value. Try again" << std::endl;
+		}
+	}
+	size_t clientNumber = atoi(argv[1]);
+	
+
+
 	CBank* bank = new CBank();
-	CBankClient* client1 = bank->CreateClient();
-	CBankClient* client2 = bank->CreateClient();
+	//CBankClient* client1 = bank->CreateClient();
+	//CBankClient* client2 = bank->CreateClient();
+
+	std::vector<CBankClient*> clients;
+
+	for (size_t i = 0; i != clientNumber; ++i)
+	{
+		clients.push_back(bank->CreateClient());
+	}
+
 
 	while (true)
 	{
 		WaitForMultipleObjects(static_cast<DWORD>(bank->GetNumberClients()), bank->GetHandles(), TRUE, INFINITE);
 	}
 
-    return 0;
+    return EXIT_SUCCESS;
 }
