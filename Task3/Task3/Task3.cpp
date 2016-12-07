dc46c20ea1a31babee84bd18957eb9970c4da410
Smi1le <tima55510@gmail.com>
2016-12-07 18:04:51 +0300

    Добавлена передача данных по pipe соединению.

--- Task3/Task3/Task3.cpp
+++ Task3/Task3/Task3.cpp
@@ -5,6 +5,8 @@
 #include <iomanip>
 #include <iostream>
 #include <random>
+#include <windows.h>
+#include <sstream>
 
 
 
@@ -22,32 +24,102 @@ namespace
 		return a + r;
 	}
 
-	void GetNumberPi(size_t iterationsNumber)
+	template <typename T>
+	std::string NumberToString(T number)
 	{
-		for (size_t itNum = 1; itNum <= iterationsNumber; ++itNum) // Перебор значения количества точек
+		std::ostringstream os;
+		os << number;
+		return os.str();
+
+	}
+
+	void TransmitDataOnServer(size_t Nmax, double Pi, std::string const &processName)
+	{
+
+		std::cout << "Go Go GO" << std::endl;
+		std::cout << "GHJITK" << std::endl;
+
+		HANDLE hPipe;
+		
+
+		std::string info = NumberToString(Nmax) + ", " + NumberToString(Pi) + ", " + processName;
+		std::cout << "info = " << info << std::endl;
+		do {
+			hPipe = CreateFile(TEXT("\\\\.\\pipe\\myPipe"),
+				GENERIC_READ | GENERIC_WRITE,
+				0,
+				NULL,
+				OPEN_EXISTING,
+				0,
+				NULL);
+		} while (hPipe == INVALID_HANDLE_VALUE);
+			 
+		std::cout << "hPipe = " << hPipe << std::endl;
+		//mutable const size_t si = info.size();
+		char buffer[1024];
+		int i = 0;
+		for (auto const &el : info)
 		{
-			double Nmax = 1.0 * itNum;
-			double Ncirc = 0;
-			for (size_t i = 0; i < Nmax; ++i)
-			{
-				double x = RandomFloat(0.f, radius);
-				double y = RandomFloat(0.f, radius);
-				if (pow(x, 2) + pow(y, 2) <= pow(radius, 2))
-				{
-					++Ncirc;
-				}
-			}
-
-			double Pi = (Ncirc / Nmax) * 4;
-
-			std::cout << Nmax << ", " << Pi << std::endl;
+			buffer[i] = el;
+			++i;
+		}
+		buffer[i] = '\0';
+		char buf = (*info.data());
+		std::cout << "sizeof(buf) = " << sizeof(buf) << std::endl;
+		std::cout << "buffer = ";
+		for (i = 0; i != info.size() + 1; i++)
+		{
+			std::cout << buffer[i];
+		}
+		std::cout << std::endl;
+		DWORD dwWritten = info.size();
+		if (hPipe != INVALID_HANDLE_VALUE)
+		{
+			std::cout << "------------->" << std::endl;
+			while (WriteFile(hPipe,
+				info.data(),
+				info.size(),   // = length of string + terminating '\0' !!!
+				&dwWritten,
+				NULL) == FALSE)
+			{ }
+			
+			std::cout << "dwWriteen = " << dwWritten << std::endl;
+			std::cout << "GHJITK" << std::endl;
+			CloseHandle(hPipe);
 		}
 	}
+
+	double GetNumberPi(size_t iterationsNumber)
+	{
+		size_t Nmax = iterationsNumber;
+		double Ncirc = 0;
+		for (size_t i = 0; i < Nmax; ++i)
+		{
+			double x = RandomFloat(0.f, radius);
+			double y = RandomFloat(0.f, radius);
+			if (pow(x, 2) + pow(y, 2) <= pow(radius, 2))
+			{
+				++Ncirc;
+			}
+		}
+
+		double Pi = (Ncirc / Nmax) * 4;
+
+		std::cout << Nmax << ", " << Pi << std::endl;
+		std::cout << "Pi = " << Pi << std::endl;
+		return Pi;
+	}
+
+
 }
 
 int main(int argc, char *argv[])
 {
 	size_t iterationsNumber = atoi(argv[1]);
-	GetNumberPi(iterationsNumber);
+	std::string processName = std::string(argv[2]);
+	//std::cout << "Pipe name = " << processName << std::endl;
+	//GetNumberPi(iterationsNumber);
+	//std::cout << "Pipe name = " << processName << std::endl;
+	TransmitDataOnServer(iterationsNumber, GetNumberPi(iterationsNumber), processName);
 	system("pause");
 }
--- Task3/Task3_main/Task3_main.cpp
+++ Task3/Task3_main/Task3_main.cpp
@@ -12,6 +12,8 @@ struct SInfoRunProcesses
 {
 	std::vector<STARTUPINFO> startUpInfos;
 	std::vector<PROCESS_INFORMATION> processInformations;
+	std::vector<bool> listFinishedProcesses;
+	HANDLE hPipe;
 	std::vector<HANDLE> handles;
 	size_t processesNumber;
 	size_t iterationsNumber;
@@ -19,8 +21,6 @@ struct SInfoRunProcesses
 
 namespace
 {
-
-
 	std::string IntToString(size_t number)
 	{
 		std::ostringstream os;
@@ -28,6 +28,41 @@ namespace
 		return os.str();
 
 	}
+
+	std::string GetName(std::string firstName, size_t number, std::string const &namePipe)
+	{
+		std::string name = firstName + IntToString(number) + namePipe;
+		std::vector<TCHAR> allName;
+		allName.resize(name.size());
+		//std::cout << allName.size() << std::endl;
+		std::copy(name.begin(), name.end(), allName.begin());
+		//TCHAR f[] = { *allName.data() };
+		return name;
+	}
+
+	bool CreatePipeForProcesses(SInfoRunProcesses &info)
+	{
+		//
+		
+		
+		info.hPipe = CreateNamedPipe("\\\\.\\pipe\\myPipe",
+			PIPE_ACCESS_DUPLEX, 
+			PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT, 
+			PIPE_UNLIMITED_INSTANCES,
+			1024, 1024, 5000, NULL);
+
+		if (info.hPipe == INVALID_HANDLE_VALUE)
+		{
+			std::cout << "*-*-*-*-*-*-*-*-*" << std::endl;
+			return false;
+		}
+
+		return true;
+	}
+
+	
+
+	
 
 	void SettingProcess(STARTUPINFO &si)
 	{
@@ -44,21 +79,23 @@ namespace
 			STARTUPINFO si;
 			PROCESS_INFORMATION pi;
 			ZeroMemory(&si, sizeof(si));
-			std::string command = "Task3.exe " + IntToString(info.iterationsNumber);
-			std::vector<TCHAR> czCommandLine;
-			czCommandLine.resize(command.size());
-			std::copy(command.begin(), command.end(), czCommandLine.begin());
+			std::string commandLine = GetName("Task3.exe ", info.iterationsNumber, " " + IntToString(i + 1));
+			
 			SettingProcess(si);
 			
-			if (!CreateProcess(NULL, LPWSTR(czCommandLine.data()), NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+			if (!CreateProcess(NULL, (LPSTR)commandLine.data(), NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
 			{
 				std::cout << "Could't create a process. Program will continue to work without it" << std::endl;
 				continue;
 			}
+
+			
 			info.startUpInfos.push_back(si);
 			info.processInformations.push_back(pi);
 			info.handles.push_back(pi.hThread);
 		}
+
+		
 
 	}
 
@@ -71,13 +108,50 @@ namespace
 		}
 	}
 
+	void WaitForMultipleProcesses(SInfoRunProcesses &info)
+	{
+		size_t countFinishedProcesses = 0;
+		while (countFinishedProcesses < info.processesNumber)
+		{
+			if (ConnectNamedPipe(info.hPipe, NULL) != FALSE)
+			{
+				std::cout << "Yes" << std::endl;
+				char buffer[1024];
+				DWORD dwRead;
+				if (ReadFile(info.hPipe, buffer, sizeof(buffer), &dwRead, NULL) == TRUE)
+				{
+					/* add terminating zero */
+					std::cout << "dwRead = " << dwRead << std::endl;
+					buffer[dwRead] = '\0';
+
+					std::cout << "buffer = " << buffer << std::endl;
+					for (size_t i = 0; i != dwRead; ++i)
+					{
+						std::cout << buffer[i] << "  --  " << static_cast<int>(buffer[i]) << std::endl;
+					}
+					std::cout << std::endl;
+					/* do something with data in buffer */
+					printf("%s", buffer);
+				}
+				std::cout << "dwRead = " << dwRead << std::endl;
+				std::cout << "buffer = " << buffer << std::endl;
+				++countFinishedProcesses;
+				
+			}
+			DisconnectNamedPipe(info.hPipe);
+		}
+	}
+
 	void Start(char *argv[])
 	{
 		SInfoRunProcesses info;
 		info.processesNumber = atoi(argv[1]);
 		info.iterationsNumber = atoi(argv[2]);
+		CreatePipeForProcesses(info);
 		RunProcess(info);
-		WaitForMultipleObjects(3, info.handles.data(), TRUE, INFINITE);
+		
+		WaitForMultipleProcesses(info);
+		//WaitForMultipleObjects(DWORD(info.processesNumber), info.handles.data(), TRUE, INFINITE);
 		CloseProcesses(info);
 	}
 }
@@ -85,7 +159,7 @@ namespace
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	if (argc < 3)
 	{
 		std::cerr << "Error! Usage program.exe <processes number> <iterations number>" << std::endl;
 		return EXIT_FAILURE;
